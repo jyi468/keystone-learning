cbe9f74152afb5f24c96b6cf01cc853e631be3ed
jyiwm <joshua.yi@warnermedia.com>
2022-04-24 11:50:53 -0400

    Lesson 2: Create relationship between users and posts. Change CMS appearance

--- keystone.ts
+++ keystone.ts
@@ -1,17 +1,35 @@
 import { config, list } from "@keystone-6/core";
-import { text } from '@keystone-6/core/fields';
+import { relationship, text } from '@keystone-6/core/fields';
+
+const lists = {
+    User: list({
+        fields: {
+            name: text({ validation: { isRequired: true }}),
+            email: text({ validation: { isRequired: true }, isIndexed: 'unique'}),
+            posts: relationship({ ref: 'Post.author', many: true }) // User can have many posts
+        }
+    }),
+    Post: list({
+        fields: {
+            title: text(),
+            author: relationship({
+                 ref: 'User.posts',
+                 ui: {
+                     displayMode: 'cards',
+                     cardFields: ['name', 'email'],
+                     inlineEdit: { fields: ['name', 'email'] },
+                     linkToItem: true,
+                     inlineCreate: { fields: ['name', 'email'] },
+                 }
+            }) // Post can only have one author
+        }
+    })
+};
 
 export default config({
     db: {
         provider: 'sqlite',
         url: 'file:./keystone.db',
     },
-    lists: {
-        User: list({
-            fields: {
-                name: text({ validation: { isRequired: true }}),
-                email: text({ validation: { isRequired: true }, isIndexed: 'unique'}),
-            }
-        })
-    },
+    lists,
 });
